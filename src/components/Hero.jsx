c0d4dad0bf6e299bc36dec1bd0a07c985d8bbd11
mati <yossefmatrefya@gmail.com>
2026-08-18 00:20:06 +0300

    Revert "video display"
    This reverts commit 7b7e2d925c83f3e9665620d649c5f13d738971ed.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -59,7 +59,7 @@ function VideoBackdrop() {
       {showVideo && (
         <video
           ref={videoRef}
-          className="h-full w-full object-contain sm:object-cover sm:object-top"
+          className="h-full w-full object-cover object-top"
           src={site.heroVideo}
           poster={site.heroPoster || undefined}
           autoPlay
@@ -75,7 +75,7 @@ function VideoBackdrop() {
         <img
           src={site.heroPoster}
           alt=""
-          className="h-full w-full object-contain drift sm:object-cover sm:object-top"
+          className="h-full w-full object-cover object-top drift"
           onError={() => setPosterFailed(true)}
         />
       )}
